563ed6674bb4ecfc4f37308cfac903aa2282c984
Pruszyn5112 <pruszynbusiness@gmail.com>
2024-02-28 22:57:31 +0100

    Add project files.

---
+++ KlocX/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace KlocX.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
