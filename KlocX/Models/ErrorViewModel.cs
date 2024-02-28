7e64ddeed398e30595f59e2227ac884bc7920dee
Pruszyn5112 <pruszynbusiness@gmail.com>
2024-02-28 23:21:11 +0100

    Revert "Database setup."
    This reverts commit b2a40a47e1e64897d5b0495b8c86d0b832960ff5.

--- KlocX/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace KlocX.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
